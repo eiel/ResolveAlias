678d02cefcd7bca040128c8026ace54830a70b8a
Tomohiko Himura <eiel.hal@gmail.com>
2017-08-09 08:37:40 +0900

    reformated switf3

--- ResolveAlias/main.swift
+++ ResolveAlias/main.swift
@@ -1,41 +1,44 @@
 import Foundation
 
-func resolveFinderAlias(url:NSURL) -> String? {
-    
-    var isAlias : AnyObject?
+func resolveFinderAlias(_ url: URL) -> String? {
+
+    var isAliasOfAny: AnyObject?
     do {
-        try url.getResourceValue(&isAlias, forKey: NSURLIsAliasFileKey)
-        if isAlias as! Bool {
+        try (url as NSURL).getResourceValue(&isAliasOfAny, forKey: URLResourceKey.isAliasFileKey)
+        guard let isAlias = isAliasOfAny as? Bool else {
+            return nil
+        }
+        if isAlias {
             do {
-                let original = try NSURL(byResolvingAliasFileAtURL: url, options: NSURLBookmarkResolutionOptions())
-                return original.path!
+                let original = try URL(resolvingAliasFileAt: url, options: NSURL.BookmarkResolutionOptions())
+                return original.path
             } catch let error as NSError {
                 print(error)
             }
         }
     } catch _ {}
-    
+
     return nil
 }
 
-class StandardErrorOutputStream: OutputStreamType {
-    func write(string: String) {
-        let stderr = NSFileHandle.fileHandleWithStandardError()
-        if let odata = string.dataUsingEncoding(NSUTF8StringEncoding) {
-            stderr.writeData(odata)
+class StandardErrorOutputStream: TextOutputStream {
+    func write(_ string: String) {
+        let stderr = FileHandle.standardError
+        if let odata = string.data(using: String.Encoding.utf8) {
+            stderr.write(odata)
         }
     }
 }
 
-let arguments :[String] = Process.arguments
+let arguments: [String] = CommandLine.arguments
 
 let filePath = arguments[1]
-let aliasURL = NSURL.fileURLWithPath(filePath)
+let aliasURL = URL(fileURLWithPath: filePath)
 
 if let url = resolveFinderAlias(aliasURL) {
     print(url)
 } else {
     var standardError = StandardErrorOutputStream()
-    print("Not alias file: \(filePath)", toStream: &standardError)
+    print("Not alias file: \(filePath)", to: &standardError)
     exit(1)
 }
